4dd9a40fc9d1627ca12a38c1e4a895de3a88cad4
sameer <sameer.smartitventures@gmail.com>
2020-04-18 18:49:41 +0530

    worked on logout

--- resources/views/web/student/dashboard/layout/sidebar.blade.php
+++ resources/views/web/student/dashboard/layout/sidebar.blade.php
@@ -6,8 +6,15 @@
 			<li><a href="#"><img class="margin-right-15" src="{{asset('web/studentdashboard/images/support.png')}}" alt="" /> Support </a></li>
 			<li><a href="#"><img class="margin-right-15" src="{{asset('web/studentdashboard/images/results_Studentdashicon.png')}}" alt="" /> Results </a></li>
 			<li><a href="#"><img class="margin-right-15" src="{{asset('web/studentdashboard/images/documents_Studentdashicon.png')}}" alt="" /> Documents </a></li>
-			<li><a href="#"><img class="margin-right-15" src="{{asset('web/studentdashboard/images/Downloadsicon.png')}}" alt="" /> Downloads </a></li>
+			<li><a href="#"><img class="margin-right-15" src="{{asset('web/studentdashboard/images/Downloadsicon.png')}}" alt="" /> Welcome, {{ Auth::user()->name }} </a></li>
+			<li><a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('frm-logout').submit();">
+    Logout
+</a> </li>
 		</ul>
+  
+<form id="frm-logout" action="{{ route('logout') }}" method="POST" style="display: none;">
+    {{ csrf_field() }}
+</form>
 	</div>
 
 	
