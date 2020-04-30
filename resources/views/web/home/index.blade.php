2154d36f55c6f68f2b235e3a59e7ac5c8e65af66
Mohammad Gulam Mohiuddin <mohd.gulam@yngmedia.com>
2020-04-30 12:01:02 +0530

    Our Mentors Share Their Stories create popup with dynamic data

--- resources/views/web/home/index.blade.php
+++ resources/views/web/home/index.blade.php
@@ -32,8 +32,9 @@
         </div>
 
         <div class="row">
-            @foreach($mentors as $mentor)
-            <div class="col-md-3 col-sm-3 col-xs-12 three_row_section text-center">
+            @foreach($mentors as  $mentor)
+            <a href="#mentor{{$loop->iteration}}">
+            <div  class="col-md-3 col-sm-3 col-xs-12 three_row_section text-center">
                 <div class="col-xs-12 col-sm-12 col-md-12 box-shadow-gray">
                     <img src="{{asset('uploads/mentor/'.$mentor->image)}}" alt="" class="img-responsive center-block" />
                     <h3 class="title-color text-color-theme letter-uppercase">{{$mentor->first_name}} {{$mentor->last_name}}</h3>
@@ -47,9 +48,39 @@
 
                         @endif
                     </p>
-                    <a class="text-color-theme font-weight-600" href="#"><i class="fa fa-angle-right font-weight-900 font-size-18"></i> Consult Now</a>
+                    @if(Illuminate\Support\Facades\Auth::check())
+                        <a class="text-color-theme font-weight-600" href="#"><i class="fa fa-angle-right font-weight-900 font-size-18"></i> Consult Now</a>
+                    @else
+                       <a class="text-color-theme font-weight-600" href="{{route('web.student-signin')}}"><i class="fa fa-angle-right font-weight-900 font-size-18"></i> Consult Now</a>
+                    @endif                    
                 </div>
             </div>
+            </a>
+
+           <div id="mentor{{$loop->iteration}}" class="overlay">
+            <div class="popup">
+                <a class="close" href="#">&times;</a>
+                <div class="row">
+                    <div class="col-md-3">
+                        <img src="{{asset('uploads/mentor/'.$mentor->image)}}" alt="" class="img-responsive center-block" />
+                    </div>
+                    <div class="col-md-8">
+                        <h3 class="title-color text-color-theme letter-uppercase">{{$mentor->first_name}} {{$mentor->last_name}}</h3>
+                    @php 
+                     $degree = App\Mentor::find($mentor->id)->degree;
+                    @endphp
+                    <p class="date-font text-color-second">{{$mentor->job_title}}. {{$mentor->major_specialization}},
+                        @if(!empty($degree))
+                        {{$degree->name}}
+                        @else
+
+                        @endif
+                    </p>
+                        <p>{!!$mentor->detailed_bio!!}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
             @endforeach
 
 
